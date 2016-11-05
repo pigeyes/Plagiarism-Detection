49b451eb5c8bd5afc753faf3cff3875a60aee6de
Mingching <mingchingchu@gmail.com>
2016-11-05 00:42:09 -0400

    create a separate detector class

--- src/Main.java
+++ src/Main.java
@@ -1,126 +1,51 @@
-import java.io.File;
 import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.text.DecimalFormat;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Scanner;
-import java.util.Map;
 
+/**
+ * This program calculates overall similarity of one
+ * file to another according to a pre-defined synonym
+ * list.
+ *
+ * @author  Ming-Ching Chu
+ * @since   2016-11-04
+ */
 
 public class Main {
-
-    private static final String USAGE = "usage: java PlagiarismDetector.java [syns_file] [file1] [file2] [tuple_size]";
-    private static final int DEFAULT_TUPLE_SIZE = 3;
+    private static final String USAGE = "USAGE: java PlagiarismDetector.java " +
+            "[syns_file] [file_to_test] [reference_file] [tuple_size]";
+    private static final String INVALID_TUPLE_SIZE = "Tuple size should be between 1~2147483647.";
 
     public static void main(String[] args) throws IOException {
-
-        if (args.length < 3) {
+        if (args.length < 3 || args.length > 4) {
             System.out.println(USAGE);
             return;
         }
 
-        int tuple_size = DEFAULT_TUPLE_SIZE;
         String synsFileName = args[0];
-        String file1 = args[1];
-        String file2 = args[2];
-        if (args.length >= 4) {
+        String fileToTest = args[1];
+        String referenceFile = args[2];
+        int tuple_size = 0;
+        if (args.length == 4) {
             try {
                 tuple_size = Integer.parseInt(args[3]);
             } catch (Exception e) {
                 System.out.print(USAGE);
                 return;
             }
-        }
-
-        Map<String, Integer> map = new HashMap<>();
-        Set<String> mask = new HashSet<>();
-        getSyns(map, synsFileName);
-        getTupleMask(mask, map, file2, tuple_size);
-        double res = calculateMatch(file1, mask, map, tuple_size);
-        System.out.println(new DecimalFormat("#.##").format(res) + " %");
-
-    }
-
-    public static void getSyns(Map<String, Integer> map, String synsFileName) {
-        try (Scanner scanner = new Scanner(new File(synsFileName))) {
-            int group = 0;
-            while (scanner.hasNext()) {
-                String[] words = scanner.nextLine().split(" ");
-                for (String word : words) {
-                    map.put(word.toLowerCase(), group);
-                }
-                group++;
+            if (tuple_size <= 0) {
+                System.out.print(INVALID_TUPLE_SIZE);
+                return;
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void getTupleMask(Set<String> tuples, Map<String, Integer> map,
-                                    String file2, int tuple_size) throws IOException {
-
-        String[] words = readFile(file2);
-        for (int i = 0; i <= words.length - tuple_size; i++) {
-            StringBuilder key = new StringBuilder();
-            for (int k = 0; k < tuple_size; k++) {
-                String tmp = words[i + k].toLowerCase();
-                if (map.containsKey(tmp)) {
-                    key.append("#").append(map.get(tmp)).append(" ");
-                } else {
-                    key.append(tmp).append(" ");
-                }
-            }
-            tuples.add(key.toString());
-        }
-    }
-
-    public static double calculateMatch(String file1, Set<String> mask,
-                                        Map<String, Integer> map, int tuple_size) throws IOException {
-
-        String[] words = readFile(file1);
-        if (words.length < tuple_size) {
-            return 0;
         }
 
-        int totalTuple = 0;
-        int matchedTuple = 0;
-        for (int i = 0; i <= words.length - tuple_size; i++) {
-            StringBuilder key = new StringBuilder();
-            for (int k = 0; k < tuple_size; k++) {
-                String tmp = words[i + k].toLowerCase();
-                if (map.containsKey(tmp)) {
-                    key.append("#").append(map.get(tmp)).append(" ");
-                } else {
-                    key.append(tmp).append(" ");
-                }
-            }
-            if (mask.contains(key.toString())) {
-                matchedTuple++;
-            }
-            totalTuple++;
+        PlagiarismDetector detector;
+        if (args.length == 4) {
+            detector = new PlagiarismDetector(synsFileName);
+        } else {
+            detector = new PlagiarismDetector(synsFileName, tuple_size);
         }
-        return (double) (matchedTuple) / (double) (totalTuple) * 100.0;
 
-    }
-
-    public static String[] readFile(String fileName) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(fileName));
-        try {
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
-
-            while (line != null) {
-                sb.append(line);
-                sb.append("\n");
-                line = br.readLine();
-            }
-            return sb.toString().split("[^a-zA-Z]+");
-        } finally {
-            br.close();
-        }
+        double res = detector.calculateMatch(fileToTest, referenceFile);
+        System.out.println("Plagiarism score: " + new DecimalFormat("#.##").format(res) + " %");
     }
 }
---
+++ src/PlagiarismDetector.java
@@ -0,0 +1,126 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
+
+/**
+ * This is a simple plagiarism detector that calculate
+ * overall similarity of one file to another according to a
+ * given synonym list.
+ *
+ * @author  Ming-Ching Chu
+ * @since   2016-11-04
+ */
+
+public class PlagiarismDetector {
+    private static final int DEFAULT_TUPLE_SIZE = 3;
+    private int tupleSize;
+    private Map<String, String> synsMap;
+
+    PlagiarismDetector(String synsFileName) throws IOException {
+        tupleSize = DEFAULT_TUPLE_SIZE;
+        synsMap = new HashMap<>();
+        groupSyns(synsFileName);
+    }
+
+    PlagiarismDetector(String synsFileName, int size) throws IOException {
+        tupleSize = size;
+        synsMap = new HashMap<>();
+        groupSyns(synsFileName);
+    }
+
+    /* Map synonyms to the first word in each synonym group */
+    public void groupSyns(String synsFileName) throws IOException {
+        try (Scanner scanner = new Scanner(new File(synsFileName))) {
+            while (scanner.hasNext()) {
+                String[] words = scanner.nextLine().toLowerCase().split(" ");
+                if (words.length == 0) {
+                    continue;
+                }
+                for (String word : words) {
+                    synsMap.put(word, words[0]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* Generate tuple set from reference document */
+    public Set<String> getTuples(String referenceFile) throws IOException {
+        Set<String> tuples = new HashSet<>();
+        String[] words = readFile(referenceFile);
+
+        for (int i = 0; i <= words.length - tupleSize; i++) {
+            String mappedString = getMappedString(words, i);
+            tuples.add(mappedString);
+        }
+        return tuples;
+    }
+
+    /* Match tuples of file1 against file2 */
+    public double calculateMatch(String fileToTest, String referenceFile) throws IOException {
+        String[] words = readFile(fileToTest);
+        Set<String> tuples = getTuples(referenceFile);
+        int totalTuple = 0;
+        int matchedTuple = 0;
+
+        for (int i = 0; i <= words.length - tupleSize; i++) {
+            String mappedString = getMappedString(words, i);
+            if (tuples.contains(mappedString)) {
+                matchedTuple++;
+            }
+            totalTuple++;
+        }
+        return (double) (matchedTuple) / (double) (totalTuple) * 100.0;
+    }
+
+    /* Map synonyms in a tuple to group representative */
+    public String getMappedString(String[] words, int start) {
+        StringBuilder key = new StringBuilder();
+
+        for (int k = 0; k < tupleSize; k++) {
+            String tmp = words[start + k];
+            if (synsMap.containsKey(tmp)) {
+                key.append(synsMap.get(tmp)).append(" ");
+            } else {
+                key.append(tmp).append(" ");
+            }
+        }
+        return key.toString();
+    }
+
+    /* Parse words in a file into array of string */
+    public String[] readFile(String fileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine().toLowerCase();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append("\n");
+                line = br.readLine();
+            }
+            // non-english-alphabets are treated as word separator
+            return sb.toString().split("[^a-zA-Z]+");
+        } finally {
+            br.close();
+        }
+    }
+
+    public int getTupleSize() {
+        return tupleSize;
+    }
+
+    public void setTupleSize(int tupleSize) {
+        this.tupleSize = tupleSize;
+    }
+
+    public void setSyns(String synsFileName) throws IOException {
+        synsMap = new HashMap<>();
+        groupSyns(synsFileName);
+    }
+}
